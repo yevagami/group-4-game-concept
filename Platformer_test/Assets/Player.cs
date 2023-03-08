9e5ad33228be47998cd9fe8c464e870da99e430f
yevagami <antonius.adriel@gmail.com>
2023-03-08 00:19:58 -0500

    08.03.2023-00:19
    -Camera follows the player
    -The player's movement script is going through a huge overhaul

--- Platformer_test/Assets/Player.cs
+++ Platformer_test/Assets/Player.cs
@@ -6,16 +6,18 @@ public class Player : MonoBehaviour
 {
     //assigned through inspector
     [SerializeField] private Rigidbody2D player_rb2d;
+    [SerializeField] private GameObject player_camera;
     public GameObject ground_check;
 
     //player stats
     [SerializeField] private float player_maxSpeed; 
-    [SerializeField] private float player_speed;
+    [SerializeField] private float player_acceleration;
+    [SerializeField] private float player_deceleration;
+
     [SerializeField] private float player_dash_speed;
     [SerializeField] private float player_jumpingSpeed;
 
     //states
-    private Vector2 player_dir;
     private Vector2 player_movement;
 
     // Start is called before the first frame update
@@ -28,56 +30,23 @@ public class Player : MonoBehaviour
     {
         player_movement.x = Input.GetAxisRaw("Horizontal");
         player_movement.y = Input.GetAxisRaw("Vertical");
-        player_dir = getPlayerDir();
-        Debug.Log(player_dir.x);
-        //Logs();
+
+        player_camera.transform.position = new Vector3(gameObject.transform.position.x, player_camera.transform.position.y,-10);
+
     }
 
     private void FixedUpdate() {
+        #region movement
+
+        float moveSpeed = player_movement.x * player_maxSpeed;
+        float deltaSpeed = moveSpeed - player_rb2d.velocity.x;
+        float acceleration = (Mathf.Abs(moveSpeed) > 0.01f) ? player_acceleration : player_deceleration;
+        float movement = Mathf.Pow(Mathf.Abs(deltaSpeed) * acceleration, 0.8f) * Mathf.Sign(moveSpeed);
+
+        player_rb2d.AddForce(Vector2.right * movement);
         
-        //player movement
-        player_rb2d.AddForce(new Vector2(player_movement.x * player_speed, player_movement.y));
+        Debug.Log(player_rb2d.velocity.x);
 
-        //Maxes out the player's speed
-        /*
-        if(player_rb2d.velocity.x * player_movement.x > player_maxSpeed){
-            player_rb2d.velocity = new Vector2(player_maxSpeed * player_movement.x, player_rb2d.velocity.y);
-        }
-        */
-
-        //player jumping
-        if(Input.GetKey(KeyCode.Space) && groundCheck()){
-            Debug.Log("JUMPING!");
-            player_rb2d.AddForce(new Vector2(0, player_jumpingSpeed));
-        }
-
-        if(Input.GetKeyDown(KeyCode.LeftShift)){
-            player_rb2d.velocity = new Vector2((player_rb2d.velocity.x + player_dash_speed) * player_dir.x, player_rb2d.velocity.y);
-        }
+        #endregion
     }
-
-    bool groundCheck(){
-        
-        if(Physics2D.BoxCast(ground_check.transform.position, new Vector2(0.3f, 0.3f), 0, Vector2.down, 0.1f)){
-            //Debug.Log("CAN JUMP");
-            return true;
-        };
-        return false;
-    }
-
-    Vector2 getPlayerDir(){
-        Vector2 playerDir = new Vector2();
-        if(player_movement.x != 0){
-            playerDir.x = player_movement.x;
-        }
-        playerDir.y = player_movement.y;
-
-        return playerDir;
-    }
-
-    /*
-    void Logs(){
-        Debug.Log(player_rb2d.velocity);
-    }
-    */
 }
